874a1c1238375e79f4bfa40a50ae81f3ee329045
Shaheen Salma <shaheencodex@gmail.com>
2025-01-24 15:00:39 +0530

    Nav Scroll and Solutions

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@ if (checkReplace !== null) {
         autoRun: true,
     })
 }
+
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('bg-dark');
+            navbar.classList.add('border-bottom');
+            navbar.classList.add('border-secondary');
+            navbar.classList.add('navbar-sticky');
+        }
+        else {
+            navbar.classList.remove('bg-dark'); 
+            navbar.classList.remove('border-bottom'); 
+            navbar.classList.remove('border-secondary'); 
+            navbar.classList.remove('navbar-sticky'); 
+        }
+    })
+}
+
+
+
+document.addEventListener('DOMContentLoaded', userScroll);
